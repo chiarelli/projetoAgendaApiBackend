07b8afdc9fef7a56af72f79dc7a9d2d9ebe757b6
Raphael Mathias <oi.sucesso@gmail.com>
2025-05-26 19:08:41 -0300

    First load on database when starting container

--- src/main/resources/database/import.sql
+++ src/main/resources/database/import.sql
@@ -1,4 +1,12 @@
--- CREATE EXTENSION IF NOT EXISTS "pgcrypto";
+\connect agendaapi;
+
+CREATE TABLE IF NOT EXISTS public.tb_categoria
+(
+    id uuid NOT NULL,
+    nome character varying(50) COLLATE pg_catalog."default" NOT NULL,
+    CONSTRAINT tb_categoria_pkey PRIMARY KEY (id),
+    CONSTRAINT ukat5cf41guxjf4dkdeo5qxva9 UNIQUE (nome)
+);
 
 INSERT INTO public.tb_categoria(id, nome) VALUES (gen_random_uuid(), 'Familia');
 INSERT INTO public.tb_categoria(id, nome) VALUES (gen_random_uuid(), 'Trabalho');
