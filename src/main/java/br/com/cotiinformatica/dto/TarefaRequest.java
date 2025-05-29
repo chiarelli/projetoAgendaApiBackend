adc876af8039b1da510e573d273bf55cd02d3f48
Raphael Mathias <oi.sucesso@gmail.com>
2025-05-28 22:05:33 -0300

    Changing Task DTO Attributes

--- src/main/java/br/com/cotiinformatica/dto/TarefaRequest.java
+++ src/main/java/br/com/cotiinformatica/dto/TarefaRequest.java
@@ -1,78 +1,66 @@
 package br.com.cotiinformatica.dto;
 
-import java.util.Date;
 import java.util.UUID;
+
+import com.fasterxml.jackson.annotation.JsonProperty;
 
 import jakarta.validation.constraints.NotBlank;
 import jakarta.validation.constraints.NotNull;
+import jakarta.validation.constraints.Pattern;
+import jakarta.validation.constraints.Size;
 
 public class TarefaRequest {
-	
-	@NotNull
-	private UUID id;
 
-	@NotBlank
+	@NotBlank(message = "O título é obrigatório.")
+	@Size(min = 8, max = 150, message = "O título deve ter entre 8 e 150 caracteres.")
 	private String titulo;
 
-	@NotNull
-	private Date data_hora;
+	@NotBlank(message = "A data é obrigatória.")
+	@Pattern(
+		regexp = "^\\d{4}-\\d{2}-\\d{2}$", 
+		message = "A data deve estar no formato AAAA-MM-DD."
+	)
+	private String data;
+
+	@NotBlank(message = "A hora é obrigatória.")
+	@Pattern(
+		regexp = "^\\d{2}:\\d{2}$",
+		message = "A hora deve estar no formato HH:MM."
+	)
+	private String hora;
 
 	private boolean finalizado;
+
+	@NotNull(message = "A categoria é obrigatória.")
+	@JsonProperty("categoria_id")
+	private UUID categoriaId;
 	
-	@NotBlank
-	private String categoria_name;
+	public TarefaRequest() { super(); }
 	
-	public TarefaRequest() {
+	public TarefaRequest(String titulo, String data, String hora, boolean finalizado, UUID categoriaId) {
 		super();
-	}
-
-	public TarefaRequest(UUID id, String titulo, Date dataHora, boolean finalizado, String categoria) {
-		super();
-		this.id = id;
 		this.titulo = titulo;
-		this.data_hora = dataHora;
+		this.data = data;
+		this.hora = hora;
 		this.finalizado = finalizado;
-		this.categoria_name = categoria;
+		this.categoriaId = categoriaId;
 	}
 
-	public UUID getId() {
-		return id;
-	}
+	public String getTitulo() { return titulo; }
+	public void setTitulo(String titulo) { this.titulo = titulo; }
+	public String getData() { return data; }
+	public void setData(String data) { this.data = data; }
+	public String getHora() { return hora; }
+	public void setHora(String hora) { this.hora = hora; }
+	public boolean isFinalizado() { return finalizado; }
+	public void setFinalizado(boolean finalizado) { this.finalizado = finalizado; }
+	public UUID getCategoriaId() { return categoriaId; }
+	public void setCategoriaId(UUID categoriaId) { this.categoriaId = categoriaId; }
 
-	public void setId(UUID id) {
-		this.id = id;
-	}
-
-	public String getTitulo() {
-		return titulo;
-	}
-
-	public void setTitulo(String titulo) {
-		this.titulo = titulo;
-	}
-
-	public Date getData_hora() {
-		return data_hora;
-	}
-
-	public void setData_hora(Date dataHora) {
-		this.data_hora = dataHora;
-	}
-
-	public boolean getFinalizado() {
-		return finalizado;
-	}
-
-	public void setFinalizado(boolean finalizado) {
-		this.finalizado = finalizado;
-	}
-
-	public String getCategoria_name() {
-		return categoria_name;
-	}
-
-	public void setCategoria_name(String categoria) {
-		this.categoria_name = categoria;
+	@Override
+	public String toString() {
+		return "CriarTarefaRequest [titulo=" + titulo + ", data=" + data + ", hora=" + hora + ", finalizado="
+				+ finalizado + ", categoriaId=" + categoriaId + "]";
 	}
 	
 }
--- src/main/java/br/com/cotiinformatica/dto/TarefaResponse.java
+++ src/main/java/br/com/cotiinformatica/dto/TarefaResponse.java
@@ -1,6 +1,5 @@
 package br.com.cotiinformatica.dto;
 
-import java.util.Date;
 import java.util.UUID;
 
 import com.fasterxml.jackson.annotation.JsonProperty;
@@ -9,8 +8,12 @@ public class TarefaResponse {
 	private UUID id;
 	private String titulo;
 
-	@JsonProperty("data_hora")
-	private Date dataHora;
+	@JsonProperty("data")
+	private String data;
+	
+	@JsonProperty("hora")
+	private String hora;
+
 	private boolean finalizado;
 	private CategoriaResponse categoria;
 	
@@ -18,11 +21,12 @@ public class TarefaResponse {
 		super();
 	}
 
-	public TarefaResponse(UUID id, String titulo, Date data_hora, boolean finalizado, CategoriaResponse categoria) {
+	public TarefaResponse(UUID id, String titulo, String data, String hora, boolean finalizado, CategoriaResponse categoria) {
 		super();
 		this.id = id;
 		this.titulo = titulo;
-		this.dataHora = data_hora;
+		this.data = data;
+		this.hora = hora;
 		this.finalizado = finalizado;
 		this.categoria = categoria;
 	}
@@ -31,8 +35,10 @@ public class TarefaResponse {
 	public void setId(UUID id) { this.id = id; }
 	public String getTitulo() { return titulo; }
 	public void setTitulo(String titulo) { this.titulo = titulo; }
-	public Date getDataHora() { return dataHora; }
-	public void setDataHora(Date data_hora) { this.dataHora = data_hora; }
+	public String getData() { return data; }
+	public void setData(String data) { this.data = data; }
+	public String getHora() { return hora; }
+	public void setHora(String hora) { this.hora = hora; }
 	public boolean isFinalizado() { return finalizado; }
 	public void setFinalizado(boolean finalizado) { this.finalizado = finalizado; }
 	public CategoriaResponse getCategoria() { return categoria; }
